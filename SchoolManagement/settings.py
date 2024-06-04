456cb9a99cba7843a1849be85297ed730a5893c4
mufiii <mufeesrahman9795@gmail.com>
2024-06-04 16:38:19 +0530

    True

--- SchoolManagement/settings.py
+++ SchoolManagement/settings.py
@@ -144,6 +144,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 
--- teacher/views.py
+++ teacher/views.py
@@ -63,6 +63,9 @@ class TeacherClassStudentsAPIView(APIView):
     )
     
     def get(self, request):
+        logger.info(f"Request headers: {request.headers}")
+        logger.info(f"Request user: {request.user}")
+        
         if not request.user.is_authenticated:
             return Response({'message': 'You need to be logged in to perform this action.'}, status=status.HTTP_401_UNAUTHORIZED)
         user = request.user
