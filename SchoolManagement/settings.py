c5aca8a5fff5c47a49f8f4706ef1995c2af08773
mufiii <mufeesrahman9795@gmail.com>
2024-09-02 15:07:21 +0530

    jj

--- SchoolManagement/settings.py
+++ SchoolManagement/settings.py
@@ -85,22 +85,22 @@ WSGI_APPLICATION = "SchoolManagement.wsgi.application"
 
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOSTNAME"),
-        "PORT": os.getenv("DB_PORT"),
-    }
-}
-
-
-
 # DATABASES = {
-#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     "default": {
+#         "ENGINE": os.getenv("DB_ENGINE"),
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOSTNAME"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
 # }
+
+
+
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
